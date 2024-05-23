592dceef8efdea8263afed4479e2c0ff655d2372
sanskrutikhedkar9 <sanskrutikhedkar09@gmail.com>
2024-05-24 00:50:34 +0530

    second commit

---
+++ CCLAB/MongoDB App/app.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const http = require('http').Server(app);
+
+const mongoose=require('mongoose');
+
+
+mongoose.connect("mongodb+srv://sanskrutikhedkar21:newPass345@cluster0.6ye6lo6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+const User = require('./models/userModel');
+
+async function insert(){
+    await User.create({
+        name:'Sanskruti',
+        email:'sanskrutikhedkar09@gmail.com'
+    });
+}
+insert();
+
+http.listen(3000, function(){
+    console.log('Server is running');
+});
